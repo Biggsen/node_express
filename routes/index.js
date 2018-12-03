a07e9137b843df26a13b856b70d1e70a314d3fc0
Biggs Thorarensen <biggs@devillion.com>
2018-12-03 17:13:53 +0000

    header & footer includes. article page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Website' });
+});
+
+/* GET article page. */
+router.get('/article', (req, res, next) => {
+  res.render('article');
 });
 
 module.exports = router;
